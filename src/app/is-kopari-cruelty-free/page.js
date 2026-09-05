3b0ecbfc79ae812ac2694ba095b0e6b7d5d7dc07
bunnyindex-co <bunnyindex@gmail.com>
2026-09-05 19:31:56 +0000

    Add /is-kopari-cruelty-free

---
+++ src/app/is-kopari-cruelty-free/page.js
@@ -0,0 +1,14 @@
+import { getDetermination } from '@/data/determinations-wave1';
+import DeterminationPage from '@/components/DeterminationPage';
+
+const rec = getDetermination('kopari');
+
+export const metadata = {
+  title: rec.title,
+  description: rec.description,
+  alternates: { canonical: 'https://www.bunnyindex.com/is-kopari-cruelty-free' },
+};
+
+export default function Page() {
+  return <DeterminationPage rec={rec} />;
+}
